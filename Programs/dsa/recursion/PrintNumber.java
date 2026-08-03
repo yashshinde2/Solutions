a1a9a122b7c95606694937110643566e7e9138fa
yashshinde2 <shindeyash0204@gmail.com>
2026-08-03 23:36:40 +0530

    Add PrintNumber class to demonstrate recursive number printing

---
+++ Programs/dsa/recursion/PrintNumber.java
@@ -0,0 +1,20 @@
+class PrintNumber{
+
+    public static void printNum(int n){
+
+        if(n == 6){
+
+            return;
+        }
+
+        System.out.println(n);
+        printNum(n+1);
+        
+    }
+    public static void main(String[] args){
+
+        int n = 1;
+        printNum(n);
+
+    }
+}
