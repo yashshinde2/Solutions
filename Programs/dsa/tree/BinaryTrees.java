fe3ac917c557d4098e7f96ebdf0cf89345a1faf9
yashshinde2 <shindeyash0204@gmail.com>
2026-08-08 12:45:16 +0530

    Add levelOrder traversal and countofNodes methods to BinaryTrees class

--- Programs/dsa/tree/BinaryTrees.java
+++ Programs/dsa/tree/BinaryTrees.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class BinaryTrees{
 
     static class Node{
@@ -72,6 +74,52 @@ class BinaryTrees{
         System.out.print(root.data + " ");
     }
 
+    public static void levelOrder(Node root){
+
+        if(root == null){
+
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+
+            Node currNode = q.remove();
+            if(currNode == null){
+
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currNode.data + " ");
+                if(currNode.left != null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static int countofNodes(Node root){
+
+        if(root == null){
+
+            return 0;
+        }
+
+        int leftCount = countofNodes(root.left);
+        int rightCount = countofNodes(root.right);
+
+        return leftCount + rightCount + 1;
+    }
     public static void main(String[] args){
 
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -81,8 +129,9 @@ class BinaryTrees{
 
         // preOrder(root);
         // inOrder(root);
-
-        postOrder(root);
+        // postOrder(root);
+        
+        System.out.println(countofNodes(root));
 
     }
 }
