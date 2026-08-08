d529c39678903226c1860577f8027b8fb633fbb2
yashshinde2 <shindeyash0204@gmail.com>
2026-08-08 12:55:48 +0530

    Add heightofTree method to BinaryTrees class

--- Programs/dsa/tree/BinaryTrees.java
+++ Programs/dsa/tree/BinaryTrees.java
@@ -133,6 +133,19 @@ class BinaryTrees{
 
         return leftSum + rightSum + root.data;
     }
+
+    public static int heightofTree(Node root){
+
+        if(root == null){
+
+            return 0;
+        }
+
+        int leftHeight = heightofTree(root.left);
+        int rightHeight = heightofTree(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
     public static void main(String[] args){
 
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -143,9 +156,10 @@ class BinaryTrees{
         // preOrder(root);
         // inOrder(root);
         // postOrder(root);
+        // levelOrder(root);
+        // System.out.println(countofNodes(root));
+        // System.out.println(sumofNodes(root));
         
-        System.out.println(countofNodes(root));
-        System.out.println(sumofNodes(root));
-
+        System.out.println(heightofTree(root));
     }
 }
