f4bfbd9947b25e91fd15c0d164e70990b66f61dc
yashshinde2 <shindeyash0204@gmail.com>
2026-08-08 12:17:59 +0530

    Add preOrder traversal method to BinaryTrees class

--- Programs/dsa/tree/BinaryTrees.java
+++ Programs/dsa/tree/BinaryTrees.java
@@ -35,13 +35,27 @@ class BinaryTrees{
 
     }
 
+    public static void preOrder(Node root){
+
+        if(root == null){
+
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
 
     public static void main(String[] args){
 
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree bt = new BinaryTree();
         Node root = bt.BuildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+
+        preOrder(root);
 
     }
 }
