707a98580c49190c6181c0c7c392b2cab0fd7524
yashshinde2 <shindeyash0204@gmail.com>
2026-08-08 11:59:48 +0530

    BinaryTree implementations for building a binary tree

---
+++ Programs/dsa/tree/BinaryTrees.java
@@ -0,0 +1,47 @@
+class BinaryTrees{
+
+    static class Node{
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+
+        static int idx = -1;
+
+        public static Node BuildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+
+            return newNode;
+
+        }
+
+    }
+
+
+    public static void main(String[] args){
+
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.BuildTree(nodes);
+        System.out.println(root.data);
+
+    }
+}
