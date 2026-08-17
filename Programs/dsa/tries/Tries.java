c371bb1253a05df26bcd79baba03b800cc6bd9da
yashshinde2 <shindeyash0204@gmail.com>
2026-08-17 10:53:00 +0530

    Added insert function in Tries

--- Programs/dsa/tries/Tries.java
+++ Programs/dsa/tries/Tries.java
@@ -16,6 +16,27 @@ public class Tries{
 
     public static Node root = new Node();
 
+    public static void insert(String word){
+
+        Node curr = root;
+
+        for(int i = 0; i<word.length(); i++){
+
+            int idx = word.charAt(i) - 'a';
+
+            if(curr.children[idx] == null){
+
+                curr.children[idx] = new Node();
+            }
+
+            if(i == word.length()-1){
+
+                curr.children[idx].eow = true;
+            }
+            curr = curr.children[idx];
+        }
+
+    }
 
     public static void main(String[] args) {
 
